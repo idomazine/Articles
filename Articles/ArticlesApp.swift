c2bc63d21a520a6baef54a26a388af0b7d0e7c90
idomazine <org2501@gmail.com>
2025-09-07 23:37:29 +0900

    initial

---
+++ Articles/ArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesApp.swift
+//  Articles
+//
+//  Created by org on 2025/09/07.
+//
+
+import SwiftUI
+
+@main
+struct ArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArticlesTests/ArticlesTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArticlesTests.swift
+//  ArticlesTests
+//
+//  Created by org on 2025/09/07.
+//
+
+import Testing
+@testable import Articles
+
+struct ArticlesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

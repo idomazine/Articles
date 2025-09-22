9c726c69c93c2b91e6a2c80fafc37a883319e9c4
idomazine <org2501@gmail.com>
2025-09-22 17:49:55 +0900

    feat: Commentモデル追加

---
+++ Articles/Infra/SwiftData/Comment.swift
@@ -0,0 +1,26 @@
+//
+//  Comment.swift
+//  Articles
+//
+//  Created by org on 2025/09/22.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Comment {
+  @Attribute(.unique) var articleId: Int
+  var body: String
+  var createdAt: Date
+  
+  init(
+    articleId: Int,
+    body: String,
+    createdAt: Date
+  ) {
+    self.articleId = articleId
+    self.body = body
+    self.createdAt = createdAt
+  }
+}

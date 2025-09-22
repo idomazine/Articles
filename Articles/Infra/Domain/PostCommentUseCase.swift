a5225a8e891cbe6e54463a450e766026a68d846d
idomazine <org2501@gmail.com>
2025-09-22 18:15:12 +0900

    feat: PostCommentUseCase作成

---
+++ Articles/Infra/Domain/PostCommentUseCase.swift
@@ -0,0 +1,35 @@
+//
+//  PostCommentUseCase.swift
+//  Articles
+//
+//  Created by org on 2025/09/22.
+//
+
+import Foundation
+import Dependencies
+
+struct PostCommentUseCase {
+  var postComment: ((articleId: Int, body: String)) async throws -> Void
+}
+
+extension PostCommentUseCase: DependencyKey {
+  static let liveValue: PostCommentUseCase = {
+    @Dependency(\.commentRepository.addComment) var addComment
+    
+    return .init(
+      postComment: { (articleId: Int, body: String) in
+        var comment = Comment(articleId: articleId, body: body, createdAt: Date())
+        try addComment(comment)
+      }
+    )
+  }()
+  
+  static var testValue: PostCommentUseCase { liveValue }
+}
+
+extension DependencyValues {
+  var postCommentUseCase: PostCommentUseCase {
+    get { self[PostCommentUseCase.self] }
+    set { self[PostCommentUseCase.self] = newValue }
+  }
+}
